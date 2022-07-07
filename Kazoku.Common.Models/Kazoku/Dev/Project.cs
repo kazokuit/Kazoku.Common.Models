7cff7319b133d967151ecbdfdc8bb99bd553572e
Felix Wetell <felix.wetell@gmail.com>
2022-07-07 20:30:46 +0200

    Adsd first models for Kazoku.Dev

---
+++ Kazoku.Common.Models/Kazoku/Dev/Project.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace Kazoku.Common.Models.Kazoku.Dev
+{
+    public class Project
+    {
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+        
+        [JsonProperty("name")]
+        public string Name { get; set; } = string.Empty;
+        
+        [JsonProperty("image")]
+        public string? Image { get; set; }
+        
+        [JsonProperty("description")]
+        public string? Description { get; set; } = string.Empty;
+        
+        [JsonProperty("status")]
+        public Status Status { get; set; } = Status.Backlog;
+        
+        [JsonProperty("url")]
+        public string Url { get; set; } = string.Empty;
+        
+        [JsonProperty("created")]
+        public DateTimeOffset Created { get; set; } = DateTimeOffset.Now;
+
+        [JsonProperty("updated")]
+        public DateTimeOffset? Updated { get; set; }
+
+        [JsonProperty("deleted")]
+        public DateTimeOffset? Deleted { get; set; }
+
+        [JsonProperty("views")]
+        public long Views { get; set; } = 0;
+
+        [JsonProperty("shares")]
+        public long Shares { get; set; } = 0;
+    }
+}
---
+++ Kazoku.Common.Models/Kazoku/Dev/Status.cs
@@ -0,0 +1,12 @@
+﻿namespace Kazoku.Common.Models.Kazoku.Dev
+{
+    public enum Status
+    {
+        Backlog     = 0,
+        New         = 1,
+        Development = 2,
+        Done        = 3,
+        Archived    = 4,
+        Cancelled   = 5,
+    }
+}
